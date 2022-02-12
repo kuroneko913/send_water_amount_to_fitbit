a0c11211ca6f5aaff25c98cd9d900c270fc8e05d
kuroneko913 <kuroneko913@users.noreply.github.com>
2022-02-13 02:54:56 +0900

    Update mqtt-to-fitbit.py

--- raspi-mqtt-client/mqtt-to-fitbit.py
+++ raspi-mqtt-client/mqtt-to-fitbit.py
@@ -87,6 +87,6 @@ client.on_connect = on_connect         # 接続時のコールバック関数を
 client.on_disconnect = on_disconnect   # 切断時のコールバックを登録
 client.on_message = on_message         # メッセージ到着時のコールバック
 
-client.connect(broker, 1883, 60)  # 接続先は自分自身
+client.connect(broker, port, 60)  # 接続先は自分自身
 
 client.loop_forever()                  # 永久ループして待ち続ける
